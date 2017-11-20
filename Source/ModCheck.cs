4657a898dbbe356554051f31761ca631b7b9ba46
Nightinggale <Nightinggale.coding@gmail.com>
2017-11-20 20:23:18 +0100

    Split generation of return value and log output
    Using a base class with virtual methods, the code is now split as follows:
    -Calculation of true/false return value
    -Log writing based on return value
    -Errors in log when xml has invalid setup (exception based)

--- Source/ModCheck.cs
+++ Source/ModCheck.cs
@@ -2,6 +2,7 @@
 using System.Xml;
 using Verse;
 using System;
+using RimWorld;
 
 namespace ModCheck
 {
@@ -12,22 +13,6 @@ namespace ModCheck
         protected string modName;
         protected string yourMod;
         protected bool errorOnFail = false;
-
-        // check that the standard tags needed for all methods are set
-        protected bool isMandatorySet(string thisMethod)
-        {
-            if (modName.NullOrEmpty())
-            {
-                Log.Error("modCheck." + thisMethod + " used with an empty/missing modName");
-                return false;
-            }
-            if (yourMod.NullOrEmpty())
-            {
-                Log.Error("modCheck." + thisMethod + " used with an empty/missing yourMod");
-                return false;
-            }
-            return true;
-        }
 
         protected bool isModLoaded(string name)
         {
@@ -48,6 +33,112 @@ namespace ModCheck
             return -1;
         }
 
+        protected virtual bool isTestPassed()
+        {
+            Log.Error("Base isTestPassed() called");
+            return false;
+        }
+
+        protected virtual string getDefaultErrorString()
+        {
+            Log.Error("Base getDefaultErrorString() called");
+            return null;
+        }
+
+        private void writeLogEntry(bool testPassed)
+        {
+            string messageStr = null;
+
+            if (!testPassed)
+            {
+                if (errorOnFail)
+                {
+                    if (messageStr.NullOrEmpty())
+                    {
+                        messageStr = getDefaultErrorString();
+                    }
+                    Log.Error(messageStr);
+                }
+            }
+        }
+
+        protected virtual void handleError(ArgumentException ex)
+        {
+            if (ex.Message == "MissingModName")
+            {
+                Log.Error("ModCheck: used with missing or empty modName");
+            }
+            else if (ex.Message == "MissingYourMod")
+            {
+                Log.Error("ModCheck: used with missing or empty yourMod");
+            }
+        }
+
+        protected override bool ApplyWorker(XmlDocument xml)
+        {
+            try
+            {
+                if (modName.NullOrEmpty()) throw new ArgumentException("MissingModName");
+                if (yourMod.NullOrEmpty()) throw new ArgumentException("MissingYourMod");
+
+                bool testPassed = isTestPassed();
+                writeLogEntry(testPassed);
+                return testPassed;
+            }
+            catch (ArgumentException ex)
+            {
+                handleError(ex);
+                return false;
+            }
+        }
+    }
+
+    // tells if a mod is loaded
+    // returns true if it is and false if not
+    // setting incompatible to true will invert the output and change error messages
+    public class isModLoaded : ModCheckBase
+    {
+        private bool incompatible = false;
+
+        protected override bool isTestPassed()
+        {
+            bool modLoaded = isModLoaded(modName);
+
+            return (modLoaded && !incompatible) || (!modLoaded && incompatible);
+        }
+
+        protected override string getDefaultErrorString()
+        {
+            if (incompatible)
+            {
+                return "Incompatible mods in use: \"" + modName + "\" can't be used with \"" + yourMod + "\"";
+            }
+            return "Missing mod: \"" + modName + "\", needed by \"" + yourMod + "\"";
+        }
+    }
+
+    public class loadOrder : ModCheckBase
+    {
+        private bool yourModFirst = false;
+
+        protected override bool isTestPassed()
+        {
+            if (yourModFirst)
+            {
+                return isModLoadedBeforeMod(yourMod, modName);
+            }
+            return isModLoadedBeforeMod(modName, yourMod);
+        }
+
+        protected override string getDefaultErrorString()
+        {
+            if (yourModFirst)
+            {
+                return "Mod load order: \"" + yourMod + "\" needs to be loaded before \"" + modName + "\"";
+            }
+            return "Mod load order: \"" + modName + "\" needs to be loaded before \"" + yourMod + "\"";
+        }
+
         protected bool isModLoadedBeforeMod(string first, string last)
         {
             int firstIndex = getModLoadIndex(first);
@@ -59,130 +150,64 @@ namespace ModCheck
             return lastIndex == -1 || firstIndex < lastIndex;
         }
     }
-
-    // tells if a mod is loaded
-    // returns true if it is and false if not
-    // setting incompatible to true will invert the output and change error messages
-    public class isModLoaded : ModCheckBase
-    {
-        private bool incompatible = false;
-
-        protected override bool ApplyWorker(XmlDocument xml)
-        {
-            if (!isMandatorySet("isModLoaded"))
-            {
-                return false;
-            }
-
-            bool modLoaded = isModLoaded(modName);
-
-            if (modLoaded && incompatible)
-            {
-                if (errorOnFail)
-                {
-                    Log.Error("Incompatible mods in use: \"" + modName + "\" can't be used with \"" + yourMod + "\"");
-                }
-                return false;
-            }
-            else if (!modLoaded && !incompatible)
-            {
-                if (errorOnFail)
-                {
-                    Log.Error("Missing mod: \"" + modName + "\", needed by \"" + yourMod + "\"");
-                }
-                return false;
-            }
-
-            return true;
-        }
-    }
-
-    public class loadOrder : ModCheckBase
-    {
-        private bool yourModFirst = false;
-
-        protected override bool ApplyWorker(XmlDocument xml)
-        {
-            if (!isMandatorySet("loadOrder"))
-            {
-                return false;
-            }
-            if (yourModFirst)
-            {
-                // reverse order
-                string temp = modName;
-                modName = yourMod;
-                yourMod = temp;
-            }
-
-            if (isModLoadedBeforeMod(modName, yourMod))
-            {
-                return true;
-            }
-            if (errorOnFail)
-            {
-                Log.Error("Mod load order: \"" + modName + "\" needs to be loaded before \"" + yourMod + "\"");
-            }
-            return false;
-        }
-    }
     
     // check that the given version is the same or lower than the version read from the mod
     public class isVersion : ModCheckBase
     {
         private string version;
 
-        protected override bool ApplyWorker(XmlDocument xml)
+        protected override bool isTestPassed()
         {
-            if (!isMandatorySet("isVersion"))
-            {
-                return false;
-            }
-
-            if (version.NullOrEmpty())
-            {
-                Log.Error("modCheck.isVersion used with an empty/missing version");
-                return false;
-            }
-
+            Version min = getVersion(version, "MinVersionUnreadable");
             int index = getModLoadIndex(modName);
             if (index != -1)
             {
-                string readVersion = ModsConfig.ActiveModsInLoadOrder.ElementAt(index).TargetVersion;
-                string[] versions = readVersion.Split('.');
-                string[] wantedVersions = version.Split('.');
-                if (versions.Count() != wantedVersions.Count())
-                {
-                    Log.Error("ModCheck.isVersion failed to compare version tags " + version + " and " + readVersion + " for mod " + modName + " checked in mod " + yourMod);
-                    return false;
-                }
-                for (int i = 0; i < versions.Count(); i++)
-                {
-                    int readIndex = -1;
-                    int wantedIndex = -1;
-
-                    if (!Int32.TryParse(versions[i], out readIndex))
-                    {
-                        Log.Error("ModCheck.isVersion failed to understand version string " + readVersion + " while testing " + modName + " in " + yourMod);
-                        return false;
-                    }
-                    if (!Int32.TryParse(wantedVersions[i], out wantedIndex))
-                    {
-                        Log.Error("ModCheck.isVersion failed to understand version string " + readVersion + " while testing in " + yourMod);
-                        return false;
-                    }
-                    if (readIndex < wantedIndex)
-                    {
-                        if (errorOnFail)
-                        {
-                            Log.Error(yourMod + " requires " + modName + " " + wantedIndex + " but version " + readIndex + " is used");
-                        }
-                        return false;
-                    }
-                }
-                return true;
+                string currentStr = ModsConfig.ActiveModsInLoadOrder.ElementAt(index).TargetVersion;
+                Version current = getVersion(currentStr, "CurrentVersionUnreadable");
+                return current.Major >= min.Major && current.Minor >= min.Minor && current.Build >= min.Build;
             }
-            return false;
+            return true;
+        }
+
+        protected override string getDefaultErrorString()
+        {
+            int index = getModLoadIndex(modName);
+            string readVersion = ModsConfig.ActiveModsInLoadOrder.ElementAt(index).TargetVersion;
+            return yourMod + " requires " + modName + " " + version + " but version " + readVersion + " is used";
+        }
+
+        private Version getVersion(string versionString, string exceptionType)
+        {
+
+            if (versionString.NullOrEmpty() || !VersionControl.IsWellFormattedVersionString(versionString))
+            {
+                throw new ArgumentException(exceptionType);
+            }
+            try
+            {
+                return VersionControl.VersionFromString(versionString);
+            }
+            catch
+            {
+                throw new ArgumentException(exceptionType);
+            }
+        }
+        
+
+        protected override void handleError(ArgumentException ex)
+        {
+            if (ex.Message == "MinVersionUnreadable")
+            {
+                Log.Error("ModCheck.isVersion used with an invalid string. Needs the format int.int.int (gameengine has the same requirement)");
+            }
+            else if (ex.Message == "CurrentVersionUnreadable")
+            {
+                Log.Error("ModCheck.isVersion used with an invalid string. Needs the format int.int.int (gameengine has the same requirement)");
+            }
+            else
+            {
+                base.handleError(ex);
+            }
         }
     }
     
