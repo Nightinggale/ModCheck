f07f7b35b2b99c110bad047a61776b5743cf5263
Nightinggale <Nightinggale.coding@gmail.com>
2018-09-13 18:36:08 +0200

    Harmony fixes for B19

--- Source/Harmony.cs
+++ Source/Harmony.cs
@@ -59,55 +59,161 @@ namespace ModCheck
         {
             List<CodeInstruction> iList = new List<CodeInstruction>(instructions);
 
-            int iLength = iList.Count;
-            if (iLength != 33)
+#if false
+            // debug output
+            // This will generate the assumedMethod filling code, complete with indexes for each line
+            {
+                string output = "";
+                for (int i = 0; i < iList.Count; ++i)
+                {
+                    string code = iList[i].ToString();
+                    code = code.Replace("\"", "\\\"");
+
+                    output += "\n // ";
+                    if (i < 10) output += " ";
+                    output += i.ToString();
+                    output += "\nassumedMethod.Add(\"" + code + "\");";
+                }
+                Log.Warning(output);
+            }
+#endif
+
+            // first step is to verify that vanilla didn't change
+            List<string> assumedMethod = new List<string>();
+
+            //  0
+            assumedMethod.Add("ldsfld System.Collections.Generic.List`1[Verse.ModContentPack] runningMods");
+            //  1
+            assumedMethod.Add("ldsfld System.Func`2[Verse.ModContentPack,System.Collections.Generic.IEnumerable`1[Verse.PatchOperation]] <>f__am$cache0");
+            //  2
+            assumedMethod.Add("brtrue Label1");
+            //  3
+            assumedMethod.Add("ldnull NULL");
+            //  4
+            assumedMethod.Add("ldftn IEnumerable`1 <ApplyPatches>m__0(Verse.ModContentPack)");
+            //  5
+            assumedMethod.Add("newobj Void .ctor(Object, IntPtr)");
+            //  6
+            assumedMethod.Add("stsfld System.Func`2[Verse.ModContentPack,System.Collections.Generic.IEnumerable`1[Verse.PatchOperation]] <>f__am$cache0");
+            //  7
+            assumedMethod.Add("ldsfld System.Func`2[Verse.ModContentPack,System.Collections.Generic.IEnumerable`1[Verse.PatchOperation]] <>f__am$cache0 [Label1]");
+            //  8
+            assumedMethod.Add("call IEnumerable`1 SelectMany[ModContentPack,PatchOperation](IEnumerable`1, System.Func`2[Verse.ModContentPack,System.Collections.Generic.IEnumerable`1[Verse.PatchOperation]])");
+            //  9
+            assumedMethod.Add("callvirt IEnumerator`1 GetEnumerator()");
+            // 10
+            assumedMethod.Add("stloc.1 NULL");
+            // 11
+            assumedMethod.Add("br Label2 [EX_BeginException]");
+            // 12
+            assumedMethod.Add("ldloc.1 NULL [Label5]");
+            // 13
+            assumedMethod.Add("callvirt Verse.PatchOperation get_Current()");
+            // 14
+            assumedMethod.Add("stloc.0 NULL");
+            // 15
+            assumedMethod.Add("ldloc.0 NULL [EX_BeginException]");
+            // 16
+            assumedMethod.Add("ldarg.0 NULL");
+            // 17
+            assumedMethod.Add("callvirt Boolean Apply(System.Xml.XmlDocument)");
+            // 18
+            assumedMethod.Add("pop NULL");
+            // 19
+            assumedMethod.Add("leave Label3");
+            // 20
+            assumedMethod.Add("stloc.2 NULL [EX_BeginCatch]");
+            // 21
+            assumedMethod.Add("ldstr \"Error in patch.Apply(): \"");
+            // 22
+            assumedMethod.Add("ldloc.2 NULL");
+            // 23
+            assumedMethod.Add("call System.String Concat(System.Object, System.Object)");
+            // 24
+            assumedMethod.Add("ldc.i4.0 NULL");
+            // 25
+            assumedMethod.Add("call Void Error(System.String, Boolean)");
+            // 26
+            assumedMethod.Add("leave Label4 [EX_EndException]");
+            // 27
+            assumedMethod.Add("ldloc.1 NULL [Label2, Label3, Label4]");
+            // 28
+            assumedMethod.Add("callvirt Boolean MoveNext()");
+            // 29
+            assumedMethod.Add("brtrue Label5");
+            // 30
+            assumedMethod.Add("leave Label6");
+            // 31
+            assumedMethod.Add("ldloc.1 NULL [EX_BeginFinally]");
+            // 32
+            assumedMethod.Add("brfalse Label7");
+            // 33
+            assumedMethod.Add("ldloc.1 NULL");
+            // 34
+            assumedMethod.Add("callvirt Void Dispose()");
+            // 35
+            assumedMethod.Add("endfinally NULL [Label7, EX_EndException]");
+            // 36
+            assumedMethod.Add("ret NULL [Label6]");
+
+
+            // test if the loaded method is identical to the expected method
+            bool bFoundCorrectMethod = true;
+
+            if (iList.Count != assumedMethod.Count)
+            {
+                bFoundCorrectMethod = false;
+            }
+
+            for (int i = 0; i < iList.Count && bFoundCorrectMethod; ++i)
+            {
+                if (iList[i].ToString() != assumedMethod[i])
+                {
+                    
+                    bFoundCorrectMethod = false;
+                }
+            }
+
+            if (!bFoundCorrectMethod)
             {
                 Log.Error("[ModCheck] Internal failure patching Verse.LoadedModManager.ApplyPatches");
-                for (int i = 0; i < iLength; ++i)
+                for (int i = 0; i < iList.Count; ++i)
                 {
+                    // return the unmodified vanilla code
                     yield return iList[i];
                 }
             }
             else
             {
                 // keep the method intact, but add calls to Memory to tell when patching is started or stopped.
-                // vanilla has been nice and added just two nop operations. Even better, they are at the right place for making the calls.
-                // The nop operations are replaced with the calls here.
-                bool First = true;
                 for (int i = 0; i < iList.Count; ++i)
                 {
-                    if (iList[i].opcode == OpCodes.Nop)
+                    // using indexes is perfectly safe because it works as long as vanilla is unchanged and that's already verified at this point
+                    if (i == 15)
                     {
-                        if (First)
+                        CodeInstruction instruction = new CodeInstruction(OpCodes.Call);
+                        if (Memory.profilingEnabled)
                         {
-                            First = false;
-                            CodeInstruction instruction = new CodeInstruction(OpCodes.Call);
-                            if (Memory.profilingEnabled)
-                            {
-                                // start the timer as well as incrementing the counter
-                                instruction.operand = typeof(ModCheck.Memory).GetMethod(nameof(ModCheck.Memory.startPatchingWithTimer));
-                            }
-                            else
-                            {
-                                // no profiling, avoid the timer overhead
-                                instruction.operand = typeof(ModCheck.Memory).GetMethod(nameof(ModCheck.Memory.startPatching));
-                            }
-                            yield return instruction;
+                            // start the timer as well as incrementing the counter
+                            instruction.operand = typeof(ModCheck.Memory).GetMethod(nameof(ModCheck.Memory.startPatchingWithTimer));
                         }
                         else
                         {
-                            if (Memory.profilingEnabled)
-                            {
-                                CodeInstruction instruction = new CodeInstruction(OpCodes.Call);
-                                instruction.operand = typeof(ModCheck.Memory).GetMethod(nameof(ModCheck.Memory.endPatchingWithTimer));
-                                yield return instruction;
-                            }
+                            // no profiling, avoid the timer overhead
+                            instruction.operand = typeof(ModCheck.Memory).GetMethod(nameof(ModCheck.Memory.startPatching));
+                        }
+                        yield return instruction;
+                    }
+                    else if (i == 19)
+                    {
+                        if (Memory.profilingEnabled)
+                        {
+                            CodeInstruction instruction = new CodeInstruction(OpCodes.Call);
+                            instruction.operand = typeof(ModCheck.Memory).GetMethod(nameof(ModCheck.Memory.endPatchingWithTimer));
+                            yield return instruction;
                         }
                     }
-                    else
-                    {
-                        yield return iList[i];
-                    }
+                    yield return iList[i];
                 }
             }
         }
